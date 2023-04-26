f27bc02c622d6abde2ca042122aeec25a94f5fcb
Aditta Chakraborty <adittacse@gmail.com>
2023-04-27 05:01:12 +0600

    social information added in right sidebar

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Button} from "react-bootstrap";
-import { FaGoogle, FaGithub } from "react-icons/fa";
+import {Button, ListGroup} from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const RightNav = () => {
     return (
         <div>
-            <h4>Login With</h4>
-            <Button className="mb-2" variant="outline-primary"><FaGoogle /> Login With Google</Button>
-            <Button variant="outline-dark"><FaGithub /> Login With Github</Button>
+            <div className="mb-4">
+                <h4 className="mb-3">Login With</h4>
+                <Button className="mb-2" variant="outline-primary"><FaGoogle /> Login With Google</Button>
+                <Button variant="outline-dark"><FaGithub /> Login With Github</Button>
+            </div>
+            <div>
+                <h4 className="mb-3">Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook className="me-2" />Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter className="me-2" />Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram className="me-2" />Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
