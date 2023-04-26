493e9e0e5c07a1d973084c19d01b501041df4a74
Aditta Chakraborty <adittacse@gmail.com>
2023-04-27 04:46:01 +0600

    login button added in right sidebar

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Button} from "react-bootstrap";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav coming</h2>
+            <h4>Login With</h4>
+            <Button className="mb-2" variant="outline-primary"><FaGoogle /> Login With Google</Button>
+            <Button variant="outline-dark"><FaGithub /> Login With Github</Button>
         </div>
     );
 };
