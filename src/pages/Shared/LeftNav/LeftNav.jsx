3872217a65b8a88d6db5bf2e0e5a5a2061814654
Aditta Chakraborty <adittacse@gmail.com>
2023-04-27 06:50:25 +0600

    all categories added in left sidebar by fetching data

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+// "../../../../public/data/categories.json"
+// "http://localhost:3000/categories"
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+    
+    useEffect( () => {
+        fetch("../../../../public/data/categories.json")
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error));
+    }, []);
+    
     return (
         <div>
-            <h2>Left Nav coming</h2>
+            <h4 className="mb-3">All Category</h4>
+            <div className="ps-4">
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/${category.id}`} className="text-secondary text-decoration-none">{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
