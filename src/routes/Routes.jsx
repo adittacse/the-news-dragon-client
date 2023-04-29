db026ab276e61ba4d2cd89a1f28debcb623a5968
Aditta Chakraborty <adittacse@gmail.com>
2023-04-29 17:23:03 +0600

    routes are updated

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,38 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Main from "../Layout/Main.jsx";
 import Home from "../pages/Home/Home/Home.jsx";
 import Category from "../pages/Home/Category/Category.jsx";
 import NewsLayout from "../Layout/NewsLayout.jsx";
 import News from "../pages/News/News.jsx";
+import LoginLayout from "../Layout/LoginLayout.jsx";
+import Login from "../pages/Login/Login/Login.jsx";
+import Register from "../pages/Login/Register/Register.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Main></Main>,
+        element: <LoginLayout></LoginLayout>,
         children: [
             {
                 path: "/",
-                element: <Category></Category>,
-                loader: () => fetch("http://localhost:3000/news/")
+                element: <Navigate to="category/0"></Navigate>
             },
             {
-                path: "/category/:id",
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
+            }
+        ]
+    },
+    {
+        path: "category",
+        element: <Main></Main>,
+        children: [
+            {
+                path: ":id",
                 element: <Category></Category>,
                 loader: ({params}) => fetch(`http://localhost:3000/categories/${params.id}`)
             }
