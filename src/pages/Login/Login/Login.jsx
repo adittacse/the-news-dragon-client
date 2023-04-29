12753e887c0aa6c9becb5144c399b27a5a28b628
Aditta Chakraborty <adittacse@gmail.com>
2023-04-29 17:36:55 +0600

    login template updated

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import {Button, Container, Form} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login</h2>
-        </div>
+        <Container className="w-25 mx-auto">
+            <h3>Login your account</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required/>
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" required />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                
+                <Button variant="primary d-block" type="submit">Login</Button>
+                <Form.Text className="text-secondary">
+                    Dont’t Have An Account ? <Link to="/register">Register</Link>
+                </Form.Text>
+                
+                <Form.Text className="d-block text-success">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                
+                <Form.Text className="text-danger">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form>
+        </Container>
     );
 };
 
--- src/pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -16,9 +16,9 @@ const NavigationBar = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
-                            <Link to="/">Home</Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/career">Career</Nav.Link>
+                            <Link to="/category/0">Home</Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#career">Career</Nav.Link>
                         </Nav>
                         <Nav>
                             {
